f5126e0d53e78ab976d731179203ebc8a6907207
jakecarlson34 <carlsoja@mail.gvsu.edu>
2021-01-27 19:59:54 -0500

    Lab2Part2

--- Lab2Part2/main.c
+++ Lab2Part2/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+    char title[255];
+    char author_name[50];
+    char ISBN[10];
+    int pages;
+    int year_published;
+}book;
+
+
 int main()
 {
-    printf("Hello world!\n");
+
     return 0;
 }
