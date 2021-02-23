375b107115051f407dfde27a539122e0bf68ee78
jakecarlson34 <carlsoja@mail.gvsu.edu>
2021-02-22 21:16:55 -0500

    Lab 5

--- Lab5part3/main.c
+++ Lab5part3/main.c
@@ -2,10 +2,11 @@
 * Author: Jake Carlson
 * Course: EGR 226 - 902
 * Date: 02/14/2021
-* Project: lab05part2
+* Project: lab05part3
 * File: main.c
 * Description: This program uses the button on the MSP432 and toggles through the Red,
-* Green, and Blue LED's using a delay function.
+* Green, and Blue LED's using a delay function and can reverse the order using a second
+* button.
 *
 **************************************************************************************/
 
@@ -17,8 +18,11 @@
  * main.c
  */
 
-uint8_t debounce();//function prototypes
-int delay();
+
+void SysTickdel(uint16_t delay);
+uint16_t debounce();//function prototypes
+
+
 
 void main(void)
 {
@@ -35,11 +39,11 @@ void main(void)
     P3->REN |= BIT2;
     P3->OUT |= BIT2;
 
-    P3->SEL0 &= ~BIT3;
-    P3->SEL1 &= ~BIT3;//setting button to output and choosing pull up resistor
-    P3->DIR &= ~BIT3;
-    P3->REN |= BIT3;
-    P3->OUT |= BIT3;
+    P5->SEL0 &= ~BIT5;
+    P5->SEL1 &= ~BIT5;//setting button 2 to output and choosing pull up resistor
+    P5->DIR &= ~BIT5;
+    P5->REN |= BIT5;
+    P5->OUT |= BIT5;
 
 
     P4->DIR |= BIT3;//green
@@ -51,76 +55,166 @@ void main(void)
 
     state = GREEN;
 
+
     while(1){
         i = debounce();// using debounce function for the button switch
         switch(state){
 
-        case GREEN: //first case using green LED
-            if((P3->IN & BIT2)== 0){//if statement for when the button is pushed and held
-                P4->OUT |= BIT3;//turn on yellow LED if button is pushed
-                j=delay();//call delay function to delay for desired amount of time
-                if(j==1){ //if statement for when button is released
-                    P4->OUT |= BIT3;//keep yellow LED on when button is released
-                }
-                else{
-                     P4->OUT &= ~BIT3;//turn LED off when switching states
-                     state = YELLOW;
-                }
-            }
-            break;
+        case GREEN: //green LED
+          if((P3->IN & BIT2)== 0){//if statement for when the button is pushed and held
+              SysTickdel(1000);
+              P4->OUT |= BIT3;//green on
 
-        case YELLOW://second case using yellow LED
-            if((P3->IN & BIT2)==0){//if statement for when the button is pushed and held
-                P4->OUT |= BIT6;//turn on yellow LED if button is pushed
-                j=delay();//delay for desired amount of time
-                if(j==1){//if statement for when button is released
-                    P4->OUT |= BIT6;//keep yellow LED on when released
-                }
-                else{
-                    P4->OUT &= ~BIT6;//turn LED off when switching states
-                    state = RED;
-                }
-            }
-            break;
+           i = debounce();
 
-        case RED://third case using blue LED
-                    if((P3->IN & BIT2)==0){//if statement for when the button is pushed and held
-                        P6->OUT |= BIT4;//turn on red LED if button is pushed
-                        j=delay();//delay for desired amount of time
-                        if(j==1){//if statement for when button is released
-                            P6->OUT |= BIT4;//keep red LED on when released
+            if(i==1){
+            P4->OUT &= ~BIT3; //green off
+            P4->OUT |= BIT6; //yellow on
+             state = YELLOW;
+           }
+            else if(i==2){
+             P4->OUT &= ~BIT3; //green off
+             P6->OUT |= BIT4; //red on
+             state = RED;
+                  }
+
+             else{
+              P4->OUT &= ~BIT3; //green off
+              P4->OUT |= BIT6; //yellow on
+               state = YELLOW;
+            }
+           }
+            else if((P5->IN & BIT5)==0){
+               SysTickdel(1000);
+               P4->OUT |= BIT3;//green on
+              if(P5->IN & BIT5){
+                   i =debounce();
+                      if(i==2){
+               P4->OUT &= ~BIT3; //green off
+               P6->OUT |= BIT4;//red on
+                  state = RED;
+              }
+           }
+             else{
+               P4->OUT &= ~BIT3; //green off
+               P6->OUT |= BIT4;//red on
+               state = RED;
+           }
+        }
+                break;
+
+
+        case YELLOW: //yellow case
+
+                if((P3->IN & BIT2)== 0){//if statement for when the button is pushed and held
+                   SysTickdel(1000);
+
+                          P4->OUT |= BIT6;//yellow on
+                          i = debounce();
+
+                         if(i==1){
+                             P4->OUT &= ~BIT6; //yellow off
+                             P6->OUT |= BIT4; //red on
+                             state = RED;
+                         }
+
+                      else{
+                          P4->OUT &= ~BIT6; //yellow off
+                          P6->OUT |= BIT4; //red on
+                          state = RED;
+                      }
+                    }
+                    else if((P5->IN & BIT5)==0){
+                        SysTickdel(1000);
+                        P4->OUT |= BIT6;//yellow on
+                        if(P5->IN & BIT5){
+                            i =debounce();
+                            if(i==2){
+                                P4->OUT &= ~BIT6; //yellow off
+                                P4->OUT |= BIT3;//green on
+                                state = GREEN;
+                            }
                         }
                         else{
-                            P6->OUT &= ~BIT4;//turn LED off when switching states
+                            P4->OUT &= ~BIT6; //yellow off
+                            P4->OUT |= BIT3;//green on
                             state = GREEN;
                         }
                     }
                     break;
 
-        }
-    }
+        case RED: //red case
+              if((P3->IN & BIT2)== 0){//if statement for when the button is pushed and held
+                  SysTickdel(1000);
+
+                     P6->OUT |= BIT4;//red on
+                     i = debounce();
+
+                    if(i==1){
+                     P6->OUT &= ~BIT4; //red off
+                     P4->OUT |= BIT3; //Green on
+                     state = GREEN;
+                                  }
+
+                              else{
+                                  P6->OUT &= ~BIT4; //red off
+                                  P4->OUT |= BIT3; //green on
+                                  state = GREEN;
+                              }
+                            }
+                            else if((P5->IN & BIT5)==0){
+                                SysTickdel(1000);
+                                P6->OUT |= BIT4;//red on
+                                if(P5->IN & BIT5){
+                                    i =debounce();
+                                    if(i==2){
+                                        P4->OUT &= ~BIT4; //red off
+                                        P4->OUT |= BIT6;//yellow on
+                                        state = YELLOW;
+                                    }
+                                }
+                                else{
+                                    P6->OUT &= ~BIT4; //red off
+                                    P4->OUT |= BIT6;//yellow on
+                                    state = YELLOW;
+                                }
+                            }
+                            break;
 
 
+      }
+   }
 }
 
-uint8_t debounce(){
-    uint8_t pinVal = 0;//making variable low
+uint16_t debounce(){
+   static uint16_t pinVal1 = 0;//making variable low
+   static uint16_t pinVal2 = 0;
 
-        if((P3->IN & BIT2) == 0){ //checking if button is pushed
-            _delay_cycles(30000);//pausing for 10 milliseconds
+        pinVal1 = (pinVal1<<1) | ((P3->IN & BIT2) / BIT2) | 0xf800;
+        pinVal2 = (pinVal2<<1) | ((P3->IN & BIT2) / BIT2) | 0xf800;
+        SysTickdel(1000);
 
-            if((P3->IN & BIT2))//checking if button still pushed
-                pinVal = 1;
-        }
+       if(pinVal1 == 0xfc00){
+           return 1;
+       }
 
-        return pinVal; //returning 0 if not pushed and 1 if pushed
+       else if(pinVal2 == 0xfc00){
+           return 2;
+       }
+
+        return 0; //returning 0 if not pushed and 1 if pushed
 }
-int delay(){
-    int i = 0;
-    for(i=0; i<=1000; i++){
-        if(P3->IN & BIT2)//for loop to used for 1 second to delay
-            return 1;
-        __delay_cycles(3000);//change this value to change delay time
-    }
-    return 0;
+void SysTickdel(uint16_t delay){
+
+    //systick init
+    SysTick->CTRL = 0;
+    SysTick->LOAD = 0x00FFFFFF;
+    SysTick->VAL = 0;
+    SysTick->CTRL = 0x00000005;
+
+
+    SysTick->LOAD = (1500 * delay);
+    SysTick->VAL = 0;
+    while((SysTick->CTRL & 0x00010000)== 0);
+    SysTick->CTRL = 0;
 }
