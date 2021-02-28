53527046c357ed571e735fd1ed1a66e5c8fb7b55
jakecarlson34 <carlsoja@mail.gvsu.edu>
2021-02-28 14:45:03 -0500

    Lab6

--- Lab4Part2/main.c
+++ Lab4Part2/main.c
@@ -29,18 +29,26 @@ void main(void)
     enum states state;
     int i=0;
     int j = 0;
-    P1->SEL0 &= ~2;
-    P1->SEL1 &= ~2;//setting button to output and choosing pull up resistor
-    P1->DIR &= ~2;
-    P1->REN |= 2;
-    P1->OUT |= 2;
+        P3->SEL1 &= ~BIT2;
+        P3->SEL0 &= ~BIT2;
+        P3->DIR &= ~BIT2;//initializing button 1
+        P3->REN |= BIT2;
+        P3->OUT |= BIT2;
 
-    P2->DIR |= 1;//setting LED's to out put
-    P2->DIR |= 2;
-    P2->DIR |= 4;
-    P2->OUT &= ~1;
-    P2->OUT &= ~2;//turning LED's off to start
-    P2->OUT &= ~4;
+        P6->SEL1 &= ~BIT4;
+        P6->SEL0 &= ~BIT4;//initializing red LED
+        P6->DIR |= BIT4;
+        P6->OUT &= ~BIT4;
+
+        P4->SEL1 &= ~BIT3;
+        P4->SEL0 &= ~BIT3;//initializing green LED
+        P4->DIR |= BIT3;
+        P4->OUT &= ~BIT3;
+
+        P4->SEL1 &= ~BIT6;
+        P4->SEL0 &= ~BIT6;//initializing yellow LED
+        P4->DIR |= BIT6;
+        P4->OUT &= ~BIT6;
 
 
     while(1){
@@ -48,42 +56,42 @@ void main(void)
         switch(state){
 
         case RED: //first case using red LED
-            if((P1->IN & 2)== 0){//if statement for when the button is pushed and held
-                P2->OUT |= 1;//turn on red LED if button is pushed
+            if((P3->IN & 2)== 0){//if statement for when the button is pushed and held
+                P4->OUT |= 3;//turn on red LED if button is pushed
                 j=delay();//call delay function to delay for desired amount of time
                 if(j==1){ //if statement for when button is released
-                    P2->OUT |= 1;//keep red LED on when button is released
+                    P4->OUT |= 3;//keep red LED on when button is released
                 }
                 else{
-                     P2->OUT &= ~1;//turn LED off when switching states
+                     P4->OUT &= ~3;//turn LED off when switching states
                      state = GREEN;
                 }
             }
             break;
 
         case GREEN://second case using green LED
-            if((P1->IN & 2)==0){//if statement for when the button is pushed and held
-                P2->OUT |= 2;//turn on green LED if button is pushed
+            if((P3->IN & 2)==0){//if statement for when the button is pushed and held
+                P6->OUT |= 6;//turn on green LED if button is pushed
                 j=delay();//delay for desired amount of time
                 if(j==1){//if statement for when button is released
-                    P2->OUT |= 2;//keep green LED on when released
+                    P4->OUT |= 6;//keep green LED on when released
                 }
                 else{
-                    P2->OUT &= ~2;//turn LED off when switching states
+                    P4->OUT &= ~6;//turn LED off when switching states
                     state = BLUE;
                 }
             }
             break;
 
         case BLUE://third case using blue LED
-                    if((P1->IN & 2)==0){//if statement for when the button is pushed and held
-                        P2->OUT |= 4;//turn on blue LED if button is pushed
+                    if((P3->IN & 2)==0){//if statement for when the button is pushed and held
+                        P6->OUT |= 4;//turn on blue LED if button is pushed
                         j=delay();//delay for desired amount of time
                         if(j==1){//if statement for when button is released
-                            P2->OUT |= 4;//keep blue LED on when released
+                            P6->OUT |= 4;//keep blue LED on when released
                         }
                         else{
-                            P2->OUT &= ~4;//turn LED off when switching states
+                            P6->OUT &= ~4;//turn LED off when switching states
                             state = RED;
                         }
                     }
@@ -98,10 +106,10 @@ void main(void)
 uint8_t debounce(){
     uint8_t pinVal = 0;//making variable low
 
-        if((P1->IN & 2) == 0){ //checking if button is pushed
+        if((P3->IN & 2) == 0){ //checking if button is pushed
             _delay_cycles(30000);//pausing for 10 milliseconds
 
-            if((P1->IN & 2))//checking if button still pushed
+            if((P3->IN & 2))//checking if button still pushed
                 pinVal = 1;
         }
 
@@ -110,7 +118,7 @@ uint8_t debounce(){
 int delay(){
     int i = 0;
     for(i=0; i<=1000; i++){
-        if(P1->IN & 2)//for loop to used for 1 second to delay
+        if(P3->IN & 2)//for loop to used for 1 second to delay
             return 1;
         __delay_cycles(3000);//change this value to change delay time
     }
