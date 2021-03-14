07a146fe9e251094f8e504a3e666a6b4bd5be4ed
jakecarlson34 <carlsoja@mail.gvsu.edu>
2021-03-14 13:45:04 -0400

    Lab 7

---
+++ Lab7part3/Debug/part3main.d
@@ -0,0 +1,49 @@
+# FIXED
+
+part3main.obj: ../part3main.c
+part3main.obj: /Applications/ti/ccs930/ccs/ccs_base/arm/include/msp.h
+part3main.obj: /Applications/ti/ccs930/ccs/ccs_base/arm/include/msp432p401r.h
+part3main.obj: /Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h
+part3main.obj: /Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h
+part3main.obj: /Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h
+part3main.obj: /Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h
+part3main.obj: /Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h
+part3main.obj: /Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h
+part3main.obj: /Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h
+part3main.obj: /Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h
+part3main.obj: /Applications/ti/ccs930/ccs/ccs_base/arm/include/msp_compatibility.h
+part3main.obj: /Applications/ti/ccs930/ccs/ccs_base/arm/include/msp432p401r_classic.h
+part3main.obj: /Applications/ti/ccs930/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+part3main.obj: /Applications/ti/ccs930/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+part3main.obj: /Applications/ti/ccs930/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+part3main.obj: /Applications/ti/ccs930/ccs/ccs_base/arm/include/system_msp432p401r.h
+part3main.obj: /Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdio.h
+part3main.obj: /Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_ti_config.h
+part3main.obj: /Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/linkage.h
+part3main.obj: /Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdarg.h
+part3main.obj: /Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdlib.h
+part3main.obj: /Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/string.h
+
+../part3main.c: 
+/Applications/ti/ccs930/ccs/ccs_base/arm/include/msp.h: 
+/Applications/ti/ccs930/ccs/ccs_base/arm/include/msp432p401r.h: 
+/Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h: 
+/Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h: 
+/Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h: 
+/Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h: 
+/Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h: 
+/Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h: 
+/Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h: 
+/Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h: 
+/Applications/ti/ccs930/ccs/ccs_base/arm/include/msp_compatibility.h: 
+/Applications/ti/ccs930/ccs/ccs_base/arm/include/msp432p401r_classic.h: 
+/Applications/ti/ccs930/ccs/ccs_base/arm/include/CMSIS/core_cm4.h: 
+/Applications/ti/ccs930/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+/Applications/ti/ccs930/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+/Applications/ti/ccs930/ccs/ccs_base/arm/include/system_msp432p401r.h: 
+/Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdio.h: 
+/Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_ti_config.h: 
+/Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/linkage.h: 
+/Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdarg.h: 
+/Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdlib.h: 
+/Applications/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/string.h: 
